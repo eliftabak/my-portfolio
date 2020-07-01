8164623f1877e6d99a0d6f0b98c0273425a6c102
eliftabak <elifchorghay@gmail.com>
2020-07-02 01:34:12 +0300

    added tabs for sidebar

--- src/components/sideNavBar/sidebar.jsx
+++ src/components/sideNavBar/sidebar.jsx
@@ -1,27 +1,56 @@
 import React from 'react';
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
 import "../../App.css";
-import "antd/dist/antd.css";
-import { Menu } from "antd";
-import {
- PieChartOutlined,
- DesktopOutlined,
- ContainerOutlined
-} from "@ant-design/icons";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import HomeIcon from '@material-ui/icons/Home';
+import PersonPinIcon from "@material-ui/icons/PersonPin";
+import EmailIcon from '@material-ui/icons/Email';
+
+
+const useStyles = makeStyles({
+ root: {
+  flexGrow: 1,
+  height: "100%",
+  paddingTop: '200px',
+  paddingBottom: '200px',
+ },
+ tabIcon: {
+  margin: 0,
+  padding: 0,
+  minWidth: '60px',
+ },
+});
+
+
 
 export default function Sidebar() {
+
+ const classes = useStyles();
+ const [value, setValue] = React.useState(0);
+
+ const handleChange = (event, newValue) => {
+  setValue(newValue);
+ };
+
  return (
   <div className="sidenav">
-   <Menu
-    className="menu-container"
-    defaultSelectedKeys={["1"]}
-    defaultOpenKeys={["sub1"]}
-    mode="inline"
-   >
-    <Menu.Item key="1" icon={<PieChartOutlined />} />
-    <Menu.Item key="2" icon={<DesktopOutlined />} />
-    <Menu.Item key="3" icon={<ContainerOutlined />} />
-   </Menu>
-   
+   <Paper square className={classes.root}>
+    <Tabs
+    className={classes.tabContainer}
+     orientation="vertical"
+     variant="scrollable"
+     value={value}
+     onChange={handleChange}
+     indicatorColor="secondary"
+     textColor="secondary"
+    >
+     <Tab className={classes.tabIcon} icon={<HomeIcon />} />
+     <Tab className={classes.tabIcon} icon={<PersonPinIcon />} />
+     <Tab className={classes.tabIcon} icon={<EmailIcon />} />
+    </Tabs>
+   </Paper>
   </div>
  )
 }
